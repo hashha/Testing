d87114f2f5a4ae6b575b3aa024e43793a478b461
hashha <harihere@ymail.com>
2012-12-10 01:58:51 -0700

    Testing

---
+++ DebuggerTutorial/src/directory_read.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class directory_read {
+	public static void main(String args[])
+	{
+		File f = new File("C:\\Users\\Murali\\Desktop\\RA\\Dump20121129\\Dump20121129");
+		
+		String[] list = f.list();
+		for(String s:list)
+		System.out.println("\\. C:\\Users\\Murali\\Desktop\\RA\\Dump20121129\\Dump20121129\\"+s);
+	}
+
+}
