8e8300180114e1ffd2f1f5f01b7fbff512719741
hashha <harihere@ymail.com>
2013-01-18 11:18:36 -0700

    testing asterisk

---
+++ DebuggerTutorial/src/testing_asterisk.java
@@ -0,0 +1,16 @@
+import org.debugger.tutorial.*;
+public class testing_asterisk {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Asterisk a = new Asterisk();
+		a.print_astrisk();
+		
+
+	}
+
+}
